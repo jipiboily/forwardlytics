5933c2a88ba3c1bfe26fe82f79b497c09d630f56
Jean-Philippe Boily <j@jipi.ca>
2016-05-29 20:12:24 -0400

    prevent timing attacks

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"crypto/subtle"
 	"net/http"
 	"os"
 )
@@ -10,7 +11,8 @@ import (
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apiKey := r.Header.Get("Forwardlytics-Api-Key")
-		if apiKey != os.Getenv("FORWARDLYTICS_API_KEY") {
+
+		if subtle.ConstantTimeCompare([]byte(os.Getenv("FORWARDLYTICS_API_KEY")), []byte(apiKey)) != 1 {
 			errorMsg := "Invalid API KEY. The Forwardlytics-Api-Key header must be specified, with the proper API key."
 			writeResponse(w, errorMsg, http.StatusUnauthorized)
 			return
